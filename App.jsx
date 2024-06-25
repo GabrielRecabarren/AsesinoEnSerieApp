fac35c74e5953d130332c0efcc9135478460659d
Gabriel Recabarren <gabriel.recabarren@gmail.com>
2024-06-25 10:03:25 -0400

    Limpieza de notification para trabjo en rama

--- App.jsx
+++ App.jsx
@@ -1,8 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Platform, Linking, Alert } from 'react-native';
-import Constants from 'expo-constants';
-import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen.jsx';
@@ -23,59 +19,9 @@ import DespedidaScreen from './src/screens/DespedidaScreen.jsx';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const notificationListener = useRef();
-  const responseListener = useRef();
-
-  useEffect(() => {
-    registerForPushNotificationsAsync().then(token => {
-      if (token) {
-        setExpoPushToken(token);
-        console.log('Expo Push Token:', token);
-      }
-    });
-
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      console.log('Notification received:', notification);
-    });
-
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log('Notification response:', response);
-    });
-
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener.current);
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
-
   return (
     <SocketProvider>
-      <NavigationContainer
-        linking={{
-          config: {},
-          async getInitialURL() {
-            const url = await Linking.getInitialURL();
-            if (url != null) {
-              return url;
-            }
-            const response = await Notifications.getLastNotificationResponseAsync();
-            return response?.notification.request.content.data.url;
-          },
-          subscribe(listener) {
-            const onReceiveURL = ({ url }) => listener(url);
-            const eventListenerSubscription = Linking.addEventListener('url', onReceiveURL);
-            const subscription = Notifications.addNotificationResponseReceivedListener(response => {
-              const url = response.notification.request.content.data.url;
-              listener(url);
-            });
-            return () => {
-              eventListenerSubscription.remove();
-              subscription.remove();
-            };
-          },
-        }}
-      >
+      <NavigationContainer>
         <UserProvider>
           <GameContextProvider>
             <PlayersContextProvider>
@@ -100,35 +46,4 @@ export default function App() {
       </NavigationContainer>
     </SocketProvider>
   );
-}
-
-async function registerForPushNotificationsAsync() {
-  let token;
-  if (Constants.isDevice) {
-    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      Alert.alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log('Token:', token);
-  } else {
-    Alert.alert('Must use physical device for Push Notifications');
-  }
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  return token;
 }
